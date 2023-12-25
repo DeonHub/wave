e452d109cc069e4e6d1ea0f8c81dd2f744381fa6
Gideon Impraim <82456010+DeonHub@users.noreply.github.com>
2023-12-25 23:43:29 +0000

    Update urls.py

--- wavex/urls.py
+++ wavex/urls.py
@@ -26,3 +26,7 @@ urlpatterns = [
     path('login/', include('login.urls')),
     path('', include('client.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
